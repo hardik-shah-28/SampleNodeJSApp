5c8edaec593fc9d3ba771c049a89cf7bf47eda43
Hardik Shah <hardikshah28.ftgmail.com>
2017-06-23 14:22:57 -0600

    changed display message to test whether it works well in azure app.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World This is Hardik Shah from Acccela!");
+    response.end("Hello World This is Hardik Shah from Acccela deploying his first app in azure!");
 
 });
 
